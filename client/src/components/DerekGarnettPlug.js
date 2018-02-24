d0e3473e9c53f7c50ca4d076d5c6523877c13fde
Derek Garnett <drkgrntt@gmail.com>
2018-02-23 18:14:03 -0600

    style derek plug

--- client/src/components/DerekGarnettPlug.js
+++ client/src/components/DerekGarnettPlug.js
@@ -3,8 +3,13 @@ import React from 'react';
 const DerekGarnettPlug = () => {
   return (
     <div className="derek">
-      <p style={{ float: 'right', marginRight: '40px' }}>
-        Powered by <a href="https://derekgarnettportfolio.firebaseapp.com">Derek Garnett</a>
+      <p style={{ textAlign: 'center', color: '#555' }}>
+        Powered by<br /><a 
+          href="https://derekgarnett.com"
+          style={{ color: '#555' }}
+        >
+          derekgarnett.com
+        </a>
       </p>
     </div>
   );
